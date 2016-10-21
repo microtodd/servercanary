d78e79768aa99ce90b81910b2ea83dac2afa598f
Todd Davenport <todd@HRT-ME-LO-P-4.local>
2016-10-21 14:56:42 -0500

    Initial Version 0.1

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # servercanary
+
+Version 0.1
+
+Listen on a port and give simple server health check messages.
+
+Particularly useful for AWS ELB health checkers.
+
+# Install
+
+- pip install psutil
+- pip install daemon
+
---
+++ canaryserver.py
@@ -0,0 +1,170 @@
+#
+# StatusCanary.py
+#
+# TODO:
+# -Add SNS notify
+#
+from wsgiref.simple_server import make_server
+from cgi import parse_qs, escape
+import psutil
+import socket
+import daemon
+
+__VERSION__ = "0.1"
+
+## StatusChecker
+#
+# Input file format:
+# command:arg
+#
+# ps    string to search for existence of in ps table
+# port  See if port is open/bound
+#
+class StatusChecker:
+
+    # List of tests to run
+    _checksToRun = []
+    _errorState = False
+    _lastError = ''
+
+    ## constructor
+    #
+    def __init__(self, inputFile=None):
+
+        # Init
+        self._checksToRun = []
+        self._errorState = False
+        self._lastError = ''
+        
+        # List of tests for this server
+        if inputFile:
+
+            # If input file could not be opened, track this as an internal error
+            try:
+                f = open(inputFile, 'r')
+                for line in f:
+                    command,arg = str(line).split(':')
+                    self._checksToRun.append( [command,arg.rstrip('\n')] )
+            except Exception as e:
+                self._errorState = True
+                self._lastError = 'Canary could not fly: ' + str(e)
+
+    ## TODO notify
+    #
+    # Send an SNS message about something
+    #
+    def notify(self):
+        pass
+
+    ## main call
+    #
+    # @param[in] none
+    # @return status A Dictionary
+    # {
+    #   status:     'ok|error',
+    #   issues:     [ list of error messages ]
+    # }
+    def checkServerHealth(self):
+
+        # Check for internal errors
+        if self._errorState:
+            returnStatus = {}
+            returnStatus['status'] = 'error'
+            returnStatus['issues'] = [self._lastError]
+            return returnStatus
+
+        # List of issues
+        serverIssues = []
+
+        # Overall status
+        serverOverallStatus = 'ok'
+
+        # Do stuff
+        for check in self._checksToRun:
+
+            # ps
+            if str(check[0]) == 'ps':
+                status = self._checkPS(check[1])
+                if not status:
+                    serverIssues.append( 'ps: could not find process named \'' + str(check[1]) + '\'' )
+
+            # port
+            elif str(check[0]) == 'port':
+                status = self._checkPort(check[1])
+                if not status:
+                    serverIssues.append( 'port: port is not bound \'' + str(check[1]) + '\'' )
+
+            # error
+            else:
+                serverIssues.append( 'Unknown command: ' + check[0] + ':' + check[1] )
+
+        # Return
+        if len(serverIssues) > 0:
+            serverOverallStatus = 'error'
+        returnStatus = {}
+        returnStatus['status'] = serverOverallStatus
+        returnStatus['issues'] = serverIssues
+        return returnStatus
+
+    ## _checkPS
+    #
+    # @param[in] string PID to check for
+    # @return boolean   Is it running
+    def _checkPS(self,process):
+        if process:
+
+            # Iterate the ps table
+            for proc in psutil.process_iter():
+                if str(proc.name).find(str(process)) != -1:
+                    return True
+
+        # Process not found
+        return False
+
+    ## _checkPort
+    #
+    # @param[in] int    portnum to check
+    # @return boolean   Is it bound
+    def _checkPort(self,port):
+        if port:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result = s.connect_ex(('127.0.0.1', int(port)))
+            if result == 0:
+                s.close()
+                return True
+
+        # Not found
+        return False
+
+# Executive
+def application(environ, start_response):
+
+    # Create our StatusChecker
+    sc = StatusChecker('foo.txt')
+
+    # Check server health
+    serverStatus = {}
+    try:
+        serverStatus = sc.checkServerHealth()
+    except Exception as e:
+        serverStatus['status'] = 'error'
+        serverStatus['issues'] = ['Couldn\'t check server health: ' + str(e)]
+    response_body = str(serverStatus)
+
+    # Check status
+    status = '200 OK'
+    if str(serverStatus['status']) != 'ok':
+        status = '500 Internal Server Error'
+
+    # Respond
+    response_headers = [('Content-Type', 'text/plain'),
+                        ('Content-Length', str(len(response_body)))]
+    start_response(status, response_headers)
+    return [response_body]
+
+# "main"
+# Run as a daemon....detach and write out the pid file
+daemon.daemonize('/tmp/canaryserver.pid')
+httpd = make_server('localhost', 8002, application)
+httpd.serve_forever()
+
